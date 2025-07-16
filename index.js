cbed9ae2f7a2751cbf43c250d0f0e9adb05e9d27
irtazakhanx <irtazakhanx2003@gmail.com>
2025-07-16 15:30:33 +0500

    changings made in directory

--- index.js
+++ index.js
@@ -2,31 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const serverless = require('serverless-http');
+
 dotenv.config();
 
 const app = express();
 
 // Middleware
-const corsOptions = {
-  origin: 'https://localhost:5173',
-  credentials: true 
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI,)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Task Manager API basic route');
+  res.send('Task Manager API');
 });
 
-const tasksRouter = require('../routes/tasks');
-const usersRouter = require('../routes/users');
+const tasksRouter = require('./routes/tasks');
+const usersRouter = require('./routes/users');
 app.use('/tasks', tasksRouter);
 app.use('/users', usersRouter);
 
@@ -34,6 +33,3 @@ const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
-
-
-module.exports = serverless(app);
