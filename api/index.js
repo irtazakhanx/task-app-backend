c4919de9a5b13085ecf8314cd1b333484c79db3b
irtazakhanx <irtazakhanx2003@gmail.com>
2025-07-16 14:39:29 +0500

    fixed vercel issues

--- api/index.js
+++ api/index.js
@@ -12,10 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
@@ -35,4 +32,4 @@ app.listen(PORT, () => {
 }); 
 
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
